07f535b13906011e59ffe47ecb059103be3b6d74
James Morris <jamesamorris@yahoo.com>
2018-11-29 11:18:42 -0500

    fix config

--- config.py
+++ config.py
@@ -1,20 +1,20 @@
 n_players = 2 # Number of players
 
-verbose = {'pay': True, #Player has a debt to pay handle_payments method
-           'buy': True, #player is buying an unowned property
-           'bstm':True,
-           'auction':True,
-           'cards':True, #when player falls on chance or community cards
+verbose = {'pay': False, #Player has a debt to pay handle_payments method
+           'buy': False, #player is buying an unowned property
+           'bstm':False,
+           'auction':False,
+           'cards':False, #when player falls on chance or community cards
            'state': True, #state information during each turn
-           'dice': True, #dice value for each turn
+           'dice': False, #dice value for each turn
            'board':False,
            'win_condition':False, #information of how winner was chosen
            'turn':False, #info about turn
-           'jail':True, #info if player goes to jail
+           'jail':False, #info if player goes to jail
            'win':True #who finally won
            }
 
-f = open("monopoly.log", "a")
+f = open("monopoly.log", "w")
 f.write("----------------------------------------------------------\n")
 
 def log(section,msg):
