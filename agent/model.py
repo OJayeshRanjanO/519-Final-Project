c63eaa41174c6493a89a90fb026218c8cca5eda8
James Morris <jamesamorris@yahoo.com>
2018-12-01 23:05:27 -0500

    add model.py

--- agent/model.py
+++ agent/model.py
@@ -20,23 +20,27 @@ class Oracle(object):
 		v2 = self.getRegression(state2)
 		return v1 - v2
 
-	def getValue(self, state, state_prev):
+	def getValue(self, state):
 		short  = self.getExpected_Short(state)
 		medium = self.getExpected_Medium(state)
 		long   = self.getExpected_Long(state)
-		rgrss  = self.getComparison(state, state_prev)
+		rgrss  = self.getRegresion(state)
 		return short + medium + long + rgrss
 
+	def getComparison(self, state, state_prev):
+		return self.getValue(state) - self.getValue(state_prev)
+
 	def action(self, modList, original_state):
+		modList = [original_state] + modList
 		f = lambda v: self.getValue(v, original_state)
 		vals = [(i, f(m)) for i, m in enumerate(modList, 0)]
 		vals = sorted(vals, key=lambda k: k[1], reverse=True)
 		return vals[0][0]
 
-
 class Agent(object):
 	def __init__(self, id):
 		self.id = id
+    self.oracle = Oracle()
 
 	def getBMSTDecision(self, state):
 		s = State(self.id, state)
@@ -46,6 +50,13 @@ class Agent(object):
 
 	def buyProperty(self, state):
 		s = State(self.id, state)
+		cost = s.getPhaseInfo()
+		ind = s.getBuyPropertyIndex()
+
+		if(s.agentAgentLiquidCash() > cost):
+			mod_s = s.clone()
+			mod_s.agentBuyProperty(ind, cost)
+      return self.oracle.action([mod_s], s) > 0
 
 	def auctionProperty(self, state):
 		s = State(self.id, state)
